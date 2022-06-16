b72ba2a0058df774cf01316e675e035454878562
Asim Ali <m.asimali69@gmail.com>
2022-06-16 11:08:58 +0000

    home page ready

--- src/pages/home.js
+++ src/pages/home.js
@@ -21,7 +21,7 @@ const Home = ({
   console.log(user)
   return (
     <>
-      <HeaderContainer bg="true" source="/images/banners/banner4.jpg" />
+      <HeaderContainer bg="true" source="/images/banners/banner-homes.jpg" />
       <FeaturedListingContainer />
       {/*<FeaturedAgentsContainer />*/}
       {/*<HomeContactContainer />*/}
