6c327e96ccc0432358760d119c355812188a6a0b
Asim Ali <m.asimali69@gmail.com>
2022-06-21 22:13:33 +0000

    logo dimensions setup

--- src/containers/FooterContainer.js
+++ src/containers/FooterContainer.js
@@ -7,7 +7,7 @@ const FooterContainer = () => {
       <Footer.Container>
         <Footer.Content>
           <Footer.Item>
-            <Footer.Title><img src="//firebasestorage.googleapis.com/v0/b/emadfyp.appspot.com/o/images%2Flogo-white.png?alt=media" width="200" height="150" /></Footer.Title>
+            <Footer.Title><img src="//firebasestorage.googleapis.com/v0/b/emadfyp.appspot.com/o/images%2Flogo-white.png?alt=media" width="250" height="100" /></Footer.Title>
             <Footer.Text>
               FCC Road, Lahore. Punjab, Pakistan, 54000
             </Footer.Text>
